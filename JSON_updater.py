6a5c213e353611164eea28a2dd6e802da8687e92
Roman Minaev <snakedom@yandex.ru>
2021-03-11 03:47:16 +0300

    killbot adjustments

--- JSON_updater.py
+++ JSON_updater.py
@@ -1,4 +1,10 @@
 from parse import GetLastEvents
+import time
 
+i = 0
 while True:
 	GetLastEvents()
+	print(f'ok')
+	print('started sleeping')
+	time.sleep(60)
+	print('woke up')
--- killbot.py
+++ killbot.py
@@ -1,6 +1,6 @@
 import discord
 import json
-import time
+import asyncio
 
 bot_token_file = open('DISCORD TOKEN_production.txt', 'r')  # TODO: change to _production
 GUILD = 'Fax'  # TODO: change to Fax
@@ -22,7 +22,7 @@ async def on_message(message):
 		return
 
 	channel = message.channel
-	if message.content.startswith('..hello'):
+	if message.content.startswith('..hello_killbot'):
 		await channel.send('Hello!')
 
 	if message.content.startswith('..init_killboard'):
@@ -31,17 +31,28 @@ async def on_message(message):
 		else:
 			await message.add_reaction('<:Godbless:808014107789754369>')
 			while True:
-				with open('lastevent.json', '') as events:
+
+				with open('lastevent.json', 'r') as events:
+					print('first open')
 					last_events_json = json.load(events)
 					last_events1 = last_events_json['last_event']
-				time.sleep(30)
+					last_event1 = last_events1[0]
+					last_event1_items = list(last_event1.items())
+
+				await asyncio.sleep(20)
+
 				with open('lastevent.json', 'r') as events:
+					print('second open')
 					last_events_json = json.load(events)
 					last_events2 = last_events_json['last_event']
+					last_event2 = last_events2[0]
+					last_event2_items = list(last_event2.items())
 
-				for i in range(len(last_events1)):
-					if last_events2[i] != last_events1[i]:
-						await channel.send(f'{last_events2[i]}')
+				for i in range(len(last_event1_items)):
+					if last_event2_items[i] != last_event1_items[i]:
+						to_send = last_event2_items[i]
+						print(f'in if {to_send}')
+						await channel.send(f'{to_send}')
 
 
 client.run(bot_token)
